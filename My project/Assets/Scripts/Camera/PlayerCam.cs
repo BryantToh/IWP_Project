d7ac7e67e278ac2c3a11b96141739dd334589a88
Ev3nts <93179644+BryantToh@users.noreply.github.com>
2024-12-02 10:20:58 +0800

    before i start lock on

--- My project/Assets/Scripts/Camera/PlayerCam.cs
+++ My project/Assets/Scripts/Camera/PlayerCam.cs
@@ -6,7 +6,7 @@ public class PlayerCam : MonoBehaviour
     public float sensY;
 
     public Transform player;
-
+    bool foundEnemy = false;
     float xRotation;
     float yRotation;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
